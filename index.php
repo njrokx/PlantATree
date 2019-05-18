7a6338905a51ca127687a7279ab7cca0143ba09b
Jock Low <jack_hammer88@hotmail.com>
2019-05-18 22:01:37 +1200

    added link to search trees

--- index.php
+++ index.php
@@ -4,40 +4,90 @@
 
 <!DOCTYPE html>
 <html>
-    <head>
-        <!--<meta http-equiv="refresh" content="0; url=TreeSearch.php">-->
-        <title>PlantATree Home</title>
-        <meta name="description" content="" />
-<meta name="keywords" content="" />
-<meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
-<meta name="robots" content="follow, index" />
-<meta property="og:site_name" content="PlantAtree" />
-<meta property="og:type" content="Article" />
-<meta property="og:title" content="PlantAtree" />
-<meta property="og:url" content="https://www.southernwoods.co.nz/buy-plants-online.php" />
+
+<head>
+    <!--<meta http-equiv="refresh" content="0; url=TreeSearch.php">-->
+    <title>PlantATree Home</title>
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
+    <meta name="robots" content="follow, index" />
+    <meta property="og:site_name" content="PlantAtree" />
+    <meta property="og:type" content="Article" />
+    <meta property="og:title" content="PlantAtree" />
+    <meta property="og:url" content="https://www.southernwoods.co.nz/buy-plants-online.php" />
 
 
-	<meta name="SKYPE_TOOLBAR" content="SKYPE_TOOLBAR_PARSER_COMPATIBLE" />
-	<meta name="format-detection" content="telephone=no" />
-	<link rel="shortcut icon" type="image/x-icon" href="https://www.southernwoods.co.nz/images/favicon.ico" />
-	<link rel="stylesheet" href="bootstrap.css" type="text/css" />
-	<link rel="stylesheet" href="https://www.southernwoods.co.nz/libraries/jquery/css/smoothness/jquery-ui.css" type="text/css" />
-    <link rel="stylesheet" type="text/css" href="stysheet1.css"/>
-    <link rel="stylesheet" type="text/css" href="stylesheet2.css"/>
-	<link rel="stylesheet" href="https://www.southernwoods.co.nz/includes/icomoon.css?1557354655" type="text/css" />
-	<link rel="stylesheet" href="stylesheet.css" type="text/css" />
-	<link rel="stylesheet" href="https://www.southernwoods.co.nz/includes/styles.css?1557354655" type="text/css" />
-	<link rel="stylesheet" href="https://www.southernwoods.co.nz/includes/skins/flat/green.css" type="text/css" />
-	<script type="text/javascript" src="https://www.southernwoods.co.nz/libraries/jquery/jquery.js"></script>
-	<script type="text/javascript" src="https://www.southernwoods.co.nz/libraries/jquery/jquery-ui.js"></script>
-	<script type="text/javascript" src="https://www.southernwoods.co.nz/includes/jquery.customSelect.js"></script>
-	<script type="text/javascript" src="https://www.southernwoods.co.nz/libraries/jquery/galleria.js"></script>
-	<script type="text/javascript" src="https://www.southernwoods.co.nz/includes/bootstrap.min.js"></script>
-	<script type="text/javascript" src="https://www.southernwoods.co.nz/includes/template.js?1557354655"></script>
-	<script type="text/javascript" src="https://www.southernwoods.co.nz/includes/icheck.js"></script>
-        
-    </head>
-    <body>
-        
-    </body>
+    <meta name="SKYPE_TOOLBAR" content="SKYPE_TOOLBAR_PARSER_COMPATIBLE" />
+    <meta name="format-detection" content="telephone=no" />
+    <link rel="shortcut icon" type="image/x-icon" href="https://www.southernwoods.co.nz/images/favicon.ico" />
+    <link rel="stylesheet" href="bootstrap.css" type="text/css" />
+    <link rel="stylesheet" href="https://www.southernwoods.co.nz/libraries/jquery/css/smoothness/jquery-ui.css" type="text/css" />
+    <link rel="stylesheet" type="text/css" href="stysheet1.css" />
+    <link rel="stylesheet" type="text/css" href="stylesheet2.css" />
+    <link rel="stylesheet" href="https://www.southernwoods.co.nz/includes/icomoon.css?1557354655" type="text/css" />
+    <link rel="stylesheet" href="stylesheet.css" type="text/css" />
+    <link rel="stylesheet" href="https://www.southernwoods.co.nz/includes/styles.css?1557354655" type="text/css" />
+    <link rel="stylesheet" href="https://www.southernwoods.co.nz/includes/skins/flat/green.css" type="text/css" />
+    <script type="text/javascript" src="https://www.southernwoods.co.nz/libraries/jquery/jquery.js"></script>
+    <script type="text/javascript" src="https://www.southernwoods.co.nz/libraries/jquery/jquery-ui.js"></script>
+    <script type="text/javascript" src="https://www.southernwoods.co.nz/includes/jquery.customSelect.js"></script>
+    <script type="text/javascript" src="https://www.southernwoods.co.nz/libraries/jquery/galleria.js"></script>
+    <script type="text/javascript" src="https://www.southernwoods.co.nz/includes/bootstrap.min.js"></script>
+    <script type="text/javascript" src="https://www.southernwoods.co.nz/includes/template.js?1557354655"></script>
+    <script type="text/javascript" src="https://www.southernwoods.co.nz/includes/icheck.js"></script>
+
+</head>
+
+<body>
+    <div id="page1">
+        <header id="header">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <div class="ecommerce-buttons-wrapper">
+                            <a class="login-link" href="/login.php">Sign In</a>
+                            <div id="Shoppingcart_cart" class="Shoppingcart_cart" style="margin: 0px;">
+                                <div class="items"><a class="view-cart-link">View Cart</a> (0)</div>
+                                <div class="show-view-cart-wrapper">
+                                    <div id="cart-summary">
+                                        <p class="num-items"><span class="update items">0</span>
+                                            item(s) in your cart</p>
+                                        <hr />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12">
+                    <a class="logo" title="Treeco"><img src="treeco.png" alt="treeco" /></a>
+                </div>
+            </div>
+            <div class="nav-wrapper">
+                <div class="nav-container">
+                    <div class="nav">
+                        <ul aria-haspopup="true" class="cmsNav1 menuStyle10">
+                            <li data-id="1"><a href="Buy_Plants_main.php" aria-haspopup="false">Buy Plants </a></li>
+                            <li data-id="2"><a href="acessories.php" aria-haspopup="false">Accessories</a></li>
+                            <li data-id="3"><a href="Category.php" aria-haspopup="false">Categories</a></li>
+                            <li data-id="4"><a href="TreeSearch.php">Search Trees</a></li>
+                            <li data-id="5"><a href="javascript:;" aria-haspopup="true">About Us</a>
+                                <ul class="menuStyle9"></ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <div class="container breadcrumb">
+            <div class="row">
+                <div class="col-xs-12"><a href="/">Home</a></div>
+            </div>
+        </div>
+    </div>
+</body>
+
 </html>
