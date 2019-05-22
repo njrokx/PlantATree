10dee89fdcbc5bf3ac24e0ed416592aabd6fa192
Jock Low <jack_hammer88@hotmail.com>
2019-05-22 15:28:48 +1200

    Update Tree list.php

--- Tree list.php
+++ Tree list.php
@@ -21,12 +21,12 @@
         echo 'Connection error: ' . mysqli_connect_error();
     }
 
-    $result = pg_query($conn, "SELECT * FROM tree_info");
+    $result = pg_query($conn, "SELECT * FROM tree_info ORDER BY category");
     if (!$result) {
         echo "An error occurred.\n";
     }
-    $trees = pg_fetch_row($result);
-    echo "$trees";
+    //$trees = pg_fetch_row($result);
+    //echo "$trees";
 
     pg_free_result($result);
 
@@ -40,13 +40,12 @@
     
     <div class="container">
         <div class="row">
-        <?php foreach($trees as $trees){ ?>
+        <?php while($row = pg_fetch_row($result)){ ?>
             
             <div class="col s6 md3">
                 <div class="card z-depth-0">
                     <div class="card content center">
-                        <img src="<?php echo htmlspecialchars($trees['img_prev'])?>" alt="">
-                        
+<!--
                         <h5>Tree Name: <?php echo htmlspecialchars($trees['name']); ?></h5>
                         <h5>Category: <?php echo htmlspecialchars($trees['category']);?></h5>
                         <h5>Soil Drainage Condition: <?php echo htmlspecialchars($trees['soil_drainage']);?></h5>
@@ -55,6 +54,9 @@
                         <h5>Max Height of Mature Tree: <?php echo htmlspecialchars($trees['max_height']);?></h5>
                         <h5>Growth Rate: <?php echo htmlspecialchars($trees['growth_rate']);?></h5>
                         <h5>Price: $<?php echo htmlspecialchars($trees['price']);?></h5>
+-->
+                        <h5>Tree Name: <?php $row[1]); ?></h5>
+                        
                     </div>
                     <div class="card-action right-align">
                         <a class="brand-text" href="#">more info</a>
