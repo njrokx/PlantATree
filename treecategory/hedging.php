a21a3adf2b8e2da7436cbfd193b8de05955ac23b
NaroNong <narol243282@gmail.com>
2019-05-22 15:50:22 +1200

    update

--- treecategory/hedging.php
+++ treecategory/hedging.php
@@ -2,22 +2,7 @@
 <html>
 <body>
 
-
-<h1>Hardwood</h1>
- 
-<img src="../images/Japanese Box Holly(1).jpg" alt="Japanese Box Holly">
-<br>  
-
-<br>
-Ideal Conditions: Full sun
-<br>
-Soil: Basic (low accidity)
-<br>
-Price: $55.00
-<br>
-Size: 0.5 - 1m
-=======
-<h1>Conifer</h1>
+<h1>Hedging</h1>
 <style>
 .container {
   position: relative;
@@ -45,19 +30,18 @@ Size: 0.5 - 1m
 }
 </style>
 <div class="container">
-    <div class="center">
+    <div class="right">
  
-    <img src="ABIES KOREANA.jpg" alt="<ABIES KOREANA.jpg"style="width:300px;height:400px;" >
-    <div class="tree-list-name"><?php echo "ABIES KOREANA"; ?></div>
+    <img src="../images/Japanese Box Holly(1).jpg" alt="Japanese Box Holly">
+    <div class="tree-list-name"><?php echo "Japanese Box Holly"; ?></div>
     <div class="tree-list-conditions"><span id="ideal">Ideal</span> Conditions: <?php echo "Heat/Drought tolerant"; ?></div>
-    <div class="tree-list-soil">Soil: <?php echo "Sandy"; ?></div>
+    <div class="tree-list-soil">Soil: <?php echo "Basic"; ?></div>
     <div class="tree-list-root">Price: <?php echo "15$"; ?></div>
-    <div class="tree-list-size">Size: <?php echo "2m - 10m"; ?></div>
+    <div class="tree-list-size">Size: <?php echo "0.5m - 1m"; ?></div>
     <div class="tree-list-info"><a href="<?php echo $row_rs_trees_list['more_info']; ?>" target="new">More info <span style="vertical-align: middle;" class="icon-mail-forward"></span></a></div>
     </div>
   
   </div>
->>>>>>> ff155ff49634c7a1f46a863eb12fd21f5dd6b7e4
 
 </body>
 </html>
