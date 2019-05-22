3fe30e48814ae44ad67793d907fb600472b5213d
NaroNong <narol243282@gmail.com>
2019-05-22 14:38:22 +1200

    plant information

--- treecategory/hedging.php
+++ treecategory/hedging.php
@@ -2,11 +2,19 @@
 <html>
 <body>
 
-<h1>Hedging</h1>
+<h1>Hardwood</h1>
+ 
+<img src="../images/Japanese Box Holly(1).jpg" alt="Japanese Box Holly">
+<br>  
 
-<?php
-echo "Hedging";
-?>
+<br>
+Ideal Conditions: Full sun
+<br>
+Soil: Basic (low accidity)
+<br>
+Price: $55.00
+<br>
+Size: 0.5 - 1m
 
 </body>
 </html>
