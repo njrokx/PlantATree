b422bb573e6eb6f31013d7b7b206ce8f1b9919f8
bakngoun <bak.ngoun@yahoo.com>
2019-05-24 08:51:43 +1200

    Update Tree list.php

--- Tree list.php
+++ Tree list.php
@@ -166,10 +166,10 @@
                             <h5>Max Height of Mature Tree: <?php echo htmlspecialchars($trees['max_height']);?></h5>
                             <h5>Growth Rate: <?php echo htmlspecialchars($trees['growth_rate']);?></h5>
                             <h5>Price: $<?php echo htmlspecialchars($trees['price']);?></h5>
-                            <a class="brand-text" href="#">More info</a><br>
                         </div>
                     </div>
                 </div>
+                <?php } ?>
             </div>
 
         </div>
